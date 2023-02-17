d0f41afe0b5a058edbdce8726e8da6a3719a5a0f
KaramMed <mkaram926@gmail.com>
2023-02-17 16:47:53 +0100

    email

--- YourAI_Project/settings.py
+++ YourAI_Project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zimsg1(^jlc&+5h97wot$47)4rvgq2p$8vfc-y%(wqa%26t$m#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
